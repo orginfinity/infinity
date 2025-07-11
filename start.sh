12f49d950ecfdfc2cd34c4e05bcb848f47005ce1
umeshrajanna <umeshmr@hotmail.com>
2025-07-11 14:25:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker build -t infinity:v1 -f Dockerfile --build-arg FILENAME=app.py --build-arg PORT=8000 .
 az acr login --name infinity.azurecr.io
-docker push infinity.azurecr.io/infinity:v1
+docker push  infinity:v1
