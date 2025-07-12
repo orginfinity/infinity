693ee85b5aafba038495a04ed04dcde0e6e20c2a
umeshrajanna <umeshmr@hotmail.com>
2025-07-12 21:03:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-# import chainlit as cl
+import chainlit as cl
 
 
-# @cl.on_message
-# async def main(message: cl.Message):
-#     # Your custom logic goes here...
+@cl.on_message
+async def main(message: cl.Message):
+    # Your custom logic goes here...
 
-#     # Send a response back to the user
-#     await cl.Message(
-#         content=f"Received: {message.content}",
-#     ).send()
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
 
 
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<html>Hello World!</html>'
+# @app.route('/')
+# def hello_world():
+#     return '<html>Hello World!</html>'
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
